b8d1b2712f5ec9891ec91b94cdb3b55d6a789c77
Monika Bugajewska <bugajewska.m@gmail.com>
2022-03-22 23:03:30 +0100

    Add two test for subtract

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,4 +8,13 @@ describe("class TurboName", function () {
 it('10 subtract 4', () => {
   expect(new TurboName(10).subtract(4).result()).toBe(6);
 });
+
+it('20 subtract 20', () => {
+  expect(new TurboName(20).subtract(20).result()).toBe(0);
 });
+it('15 subtract 25', () => {
+  expect(new TurboName(15).subtract(25).result()).toBe(-10);
+});
+
+});
+
